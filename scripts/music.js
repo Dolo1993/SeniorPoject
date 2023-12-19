90706637ea6c5bd4321b6845fd0c41236e4df6f8
Emmanuel DK Dolo <emmanueldkdolo199@gmail.com>
2023-12-19 17:24:30 +0000

    done with audio area

--- scripts/music.js
+++ scripts/music.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the audio element
     var audio = document.getElementById("background-music");
+    var startAudioButton = document.getElementById("startAudioButton");
 
-    // Check if the audio has been paused or ended
-    if (audio.paused || audio.ended) {
-        // Play the audio
-        audio.play();
+    if (audio && startAudioButton) {
+        // Check if the audio has been paused or ended
+        if (audio.paused || audio.ended) {
+            startAudioButton.addEventListener("click", function() {
+                // Play the audio
+                audio.play().then(function() {
+                    console.log("Audio played successfully.");
+                }).catch(function(error) {
+                    console.error("Error playing audio:", error);
+                });
+            });
+        }
+    } else {
+        console.error("Audio element or start button not found.");
     }
 });
